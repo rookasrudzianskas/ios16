a1e27f244469193aa2a09ce1417f787fb07581ff
Rokas <rookas.rudzenskas@gmail.com>
2022-10-26 19:26:24 +0300

    Rokas

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import {Image, ImageBackground, StyleSheet, Text, TouchableOpacity, useWindowDimensions, View} from 'react-native';
 import LockScreen from "./src/screens/LockScreen";
 import bg from './assets/images/bg.jpeg';
-import {SensorType, useAnimatedSensor, useAnimatedStyle} from "react-native-reanimated";
+import Animated, {SensorType, useAnimatedSensor, useAnimatedStyle} from "react-native-reanimated";
 
 const IMAGE_OFFSET = 300;
 
@@ -13,13 +13,23 @@ export default function App() {
     const imageStyle = useAnimatedStyle(() => {
         const {yaw, pitch, roll} = sensor.sensor.value;
         console.log("YAW >> ", yaw.toFixed(1),"PITCH >>> ", pitch.toFixed(1), "ROLL >> ", roll.toFixed(1));
-        return {};
+        return {
+            top: -200,
+            left: 0
+        };
     });
 
     return (
         <View style={styles.container}>
             {/*<LockScreen />*/}
-            <Image source={bg} style={{width: width + 2 * IMAGE_OFFSET, height: height + 2 * IMAGE_OFFSET}}/>
+            <Animated.Image source={bg} style={[
+                {
+                    width: width + 2 * IMAGE_OFFSET,
+                    height: height + 2 * IMAGE_OFFSET,
+                    position: 'absolute'
+                },
+                imageStyle
+            ]}/>
             <StatusBar style="light" />
         </View>
     );
