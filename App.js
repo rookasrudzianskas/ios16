d4968686fc0536503ec12c04ed6967e82a08bb85
Rokas <rookas.rudzenskas@gmail.com>
2022-10-26 11:47:43 +0300

    Rokas

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import {ImageBackground, StyleSheet, Text, View} from 'react-native';
 import wallpaper from './assets/images/wallpaper.webp';
+import {Ionicons} from '@expo/vector-icons';
 
 export default function App() {
     return (
         <ImageBackground source={wallpaper} style={styles.container} className="">
+            <Ionicons name="ios-lock-closed" />
+            <Text>Friday, 30 September</Text>
+            <Text>3:26PM</Text>
 
             <StatusBar style="auto" />
         </ImageBackground>
@@ -14,7 +18,6 @@ export default function App() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
     },
