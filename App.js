646483be3130753a3f1562d5a8f3d54b445d0a7a
Rokas <rookas.rudzenskas@gmail.com>
2022-10-26 18:55:51 +0300

    ROkas

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import {ImageBackground, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {Image, ImageBackground, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import LockScreen from "./src/screens/LockScreen";
+import bg from './assets/images/bg.jpeg';
+import {useAnimatedSensor} from "react-native-reanimated";
 
 export default function App() {
+
+    const sensor = useAnimatedSensor();
 
     return (
         <View style={styles.container}>
             {/*<LockScreen />*/}
-            <Text>Hello Bro!</Text>
+            <Image source={bg} style={styles.image} />
             <StatusBar style="light" />
         </View>
     );
@@ -18,5 +22,9 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
+    },
+    image: {
+        width: '100%',
+        height: '100%',
     }
 });
