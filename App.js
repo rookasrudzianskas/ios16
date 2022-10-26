2671245df4d90cab413cce11af8c633958e1431d
Rokas <rookas.rudzenskas@gmail.com>
2022-10-26 19:23:53 +0300

    Rokas

--- App.js
+++ App.js
@@ -1,17 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import {Image, ImageBackground, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {Image, ImageBackground, StyleSheet, Text, TouchableOpacity, useWindowDimensions, View} from 'react-native';
 import LockScreen from "./src/screens/LockScreen";
 import bg from './assets/images/bg.jpeg';
-import {useAnimatedSensor} from "react-native-reanimated";
+import {SensorType, useAnimatedSensor, useAnimatedStyle} from "react-native-reanimated";
+
+const IMAGE_OFFSET = 300;
 
 export default function App() {
+    const {width, height} = useWindowDimensions();
+    const sensor = useAnimatedSensor(SensorType.ROTATION, {interval: 1000});
 
-    const sensor = useAnimatedSensor();
+    const imageStyle = useAnimatedStyle(() => {
+        const {yaw, pitch, roll} = sensor.sensor.value;
+        console.log("YAW >> ", yaw.toFixed(1),"PITCH >>> ", pitch.toFixed(1), "ROLL >> ", roll.toFixed(1));
+        return {};
+    });
 
     return (
         <View style={styles.container}>
             {/*<LockScreen />*/}
-            <Image source={bg} style={styles.image} />
+            <Image source={bg} style={{width: width + 2 * IMAGE_OFFSET, height: height + 2 * IMAGE_OFFSET}}/>
             <StatusBar style="light" />
         </View>
     );
@@ -24,7 +32,6 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
     },
     image: {
-        width: '100%',
-        height: '100%',
+
     }
 });
