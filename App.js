2b494177a94d160e945a206bcb06c33a6c2631fb
Rokas <rookas.rudzenskas@gmail.com>
2022-10-26 12:52:58 +0300

    Rokas

--- App.js
+++ App.js
@@ -39,7 +39,7 @@ export default function App() {
                 </TouchableOpacity>
             </View>
 
-            <StatusBar style="auto" />
+            <StatusBar style="light" />
         </ImageBackground>
     );
 }
--- assets/data/notifications.js
+++ assets/data/notifications.js
@@ -21,8 +21,8 @@ export default [
     id: "3",
     createdAt: "45m",
     icon: mail,
-    title: "notJust.dev inquiry",
-    subtitle: "Hey Vadim, how is it going? ",
+    title: "byRookas.com inquiry",
+    subtitle: "Hey Rokas, how is it going? ",
   },
   {
     id: "4",
@@ -42,7 +42,7 @@ export default [
     id: "6",
     createdAt: "5h",
     icon: mail,
-    title: "Hola Vadim",
+    title: "Hola Rokas",
     subtitle:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing",
   },
