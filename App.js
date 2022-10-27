5633cead3a088e9983dc11d9875a5d8e544ecb67
Rokas <rookas.rudzenskas@gmail.com>
2022-10-27 09:17:40 +0300

    Rokas

--- App.js
+++ App.js
@@ -2,6 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import {Image, ImageBackground, StyleSheet, Text, TouchableOpacity, useWindowDimensions, View} from 'react-native';
 import SensorAnimatedImage from "./src/components/SensorAnimatedImage";
 import bg from './assets/images/bg.jpeg';
+// import layer1 from './assets/images/Parallax/1.png';
+import layer1 from './assets/images/Parallax/2.png';
+import layer2 from './assets/images/Parallax/3.png';
+import layer3 from './assets/images/Parallax/4.png';
+import layer4 from './assets/images/Parallax/5.png';
+import Parallax from "./src/components/Parallax";
 
 
 export default function App() {
@@ -9,7 +15,8 @@ export default function App() {
     return (
         <View style={styles.container}>
             {/*<LockScreen />*/}
-            <SensorAnimatedImage image={bg} />
+            {/*<SensorAnimatedImage image={bg} />*/}
+            <Parallax  layers={[layer1, layer2, layer3, layer4]} />
             <StatusBar style="light" />
         </View>
     );
---
+++ src/components/Parallax.js
@@ -0,0 +1,15 @@
+//@ts-nocheck
+import React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+
+const Parallax = ({ layers }) => {
+    return (
+        <View>
+            <Text>
+                byrookas 🚀
+            </Text>
+        </View>
+    );
+};
+
+export default Parallax;
